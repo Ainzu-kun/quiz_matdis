c94656af3ba812a1facd12e75b9c957d8743015d
rafidhp <rafiislamipasha27@gmail.com>
2025-05-06 20:26:23 +0700

    feat: add CharacterSeeder file

--- database/seeders/CharacterSeeder.php
+++ database/seeders/CharacterSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Character;
 
 class CharacterSeeder extends Seeder
 {
@@ -12,6 +13,23 @@ class CharacterSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $characters = [
+            [
+                'name' => 'Naruto Uzumaki',
+                'rarity_id' => 1,
+                'image_url' => 'https://example.com/image1.jpg',
+                'description' => 'Description for Character 1',
+            ],
+            [
+                'name' => 'Sasuke Uchiha',
+                'rarity_id' => 2,
+                'image_url' => 'https://example.com/image2.jpg',
+                'description' => 'Description for Character 2',
+            ],
+        ];
+
+        foreach($characters as $key => $value) {
+            Character::create($value);
+        }
     }
 }
